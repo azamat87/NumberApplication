35e55422359416615bd28f99015e397c8e6e1b69
azamat.nurkhojayev <azamat.nurkhojayev@homecredit.kz>
2022-10-14 21:18:22 +0600

    create fragments

--- app/src/main/java/com/app/numberapplication/main/presentation/MainActivity.kt
+++ app/src/main/java/com/app/numberapplication/main/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.app.numberapplication
+package com.app.numberapplication.main.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.numberapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
