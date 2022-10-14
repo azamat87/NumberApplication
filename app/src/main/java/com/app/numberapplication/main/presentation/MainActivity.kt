890e57ba65f8aacc38f1e8bd634d96ea12158dfa
azamat.nurkhojayev <azamat.nurkhojayev@homecredit.kz>
2022-10-14 21:49:20 +0600

    fragments

--- app/src/main/java/com/app/numberapplication/main/presentation/MainActivity.kt
+++ app/src/main/java/com/app/numberapplication/main/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.app.numberapplication.main.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.numberapplication.R
+import com.app.numberapplication.numbers.presentation.NumbersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, NumbersFragment())
+            .commit()
     }
 }
