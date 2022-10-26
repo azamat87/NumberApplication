b5ab16dd52b18eb3ab762268542e8fce36048230
azamat.nurkhojayev <azamat.nurkhojayev@homecredit.kz>
2022-10-26 22:13:37 +0600

    ViewModel

--- app/src/main/java/com/app/numberapplication/numbers/presentation/UiState.kt
+++ app/src/main/java/com/app/numberapplication/numbers/presentation/UiState.kt
@@ -3,17 +3,17 @@ package com.app.numberapplication.numbers.presentation
 sealed class UiState {
 
     interface Mapper<T> {
-        fun map(list: List<NumberUi>, message: String) : T
+        fun map(message: String) : T
     }
 
     abstract fun <T> map(mapper : Mapper<T>) : T
 
-    class Success(private val list: List<NumberUi> = emptyList()) : UiState() {
-        override fun <T> map(mapper: Mapper<T>): T = mapper.map(list, "")
+    class Success: UiState() {
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map( "")
     }
 
     class Error(private val message: String) : UiState() {
-        override fun <T> map(mapper: Mapper<T>): T = mapper.map(emptyList(), message)
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(message)
     }
 
 }
